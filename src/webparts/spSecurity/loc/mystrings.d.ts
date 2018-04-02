5b824d0344c542d8f8f2ca5412989e580fbf2cd1
Sylvester Wells <sylvester.wells@vanroey.pro>
2018-04-02 07:43:05 +0100

    Currently deployed version

---
+++ src/webparts/spSecurity/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpSecurityWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpSecurityWebPartStrings' {
+  const strings: ISpSecurityWebPartStrings;
+  export = strings;
+}
